454a1c83371593716d0e52e17c54997f346a9965
David Carreto Fidalgo <david.carreto.fidalgo@mpcdf.mpg.de>
2023-10-19 16:55:21 +0200

    simplify example

--- transformers/example.py
+++ transformers/example.py
@@ -1,60 +1,34 @@
-import argparse
 import math
 
-from datasets import load_dataset
+from datasets import Dataset
 from transformers import AutoTokenizer
 from transformers import DataCollatorForLanguageModeling
 from transformers import AutoModelForCausalLM, TrainingArguments, Trainer
+import os
 
 
-def main(args):
+print(os.environ)
+
+
+def main():
     model = AutoModelForCausalLM.from_pretrained("distilgpt2")
-    eli5 = load_dataset("eli5", split="train_asks[:500]")
     tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
 
-    eli5 = eli5.train_test_split(test_size=0.2)
-    eli5 = eli5.flatten()
+    ds = Dataset.from_dict({"text": [" ".join(["I will learn to repeat myself!"]*10)] * 5000})
+    ds = ds.train_test_split(test_size=0.2)
 
     def preprocess_function(examples):
-        return tokenizer([" ".join(x) for x in examples["answers.text"]])
-
-    tokenized_eli5 = eli5.map(
-        preprocess_function,
-        batched=True,
-        num_proc=4,
-        remove_columns=eli5["train"].column_names,
-    )
-
-    block_size = 128
-
-    def group_texts(examples):
-        # Concatenate all texts.
-        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
-        if total_length >= block_size:
-            total_length = (total_length // block_size) * block_size
-        # Split by chunks of block_size.
-        result = {
-            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-            for k, t in concatenated_examples.items()
-        }
+        result = tokenizer(examples["text"])
         result["labels"] = result["input_ids"].copy()
         return result
 
-    lm_dataset = tokenized_eli5.map(group_texts, batched=True, num_proc=4)
+    tokenized_ds = ds.map(preprocess_function, batched=True)
 
     tokenizer.pad_token = tokenizer.eos_token
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
-    print("Preprocessing done")
-
-    if args.preprocess:
-        return
-
     training_args = TrainingArguments(
-        output_dir="my_awesome_eli5_clm-model",
+        output_dir="model",
         evaluation_strategy="epoch",
         learning_rate=2e-5,
         weight_decay=0.01,
@@ -63,8 +37,8 @@ def main(args):
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=lm_dataset["train"],
-        eval_dataset=lm_dataset["test"],
+        train_dataset=tokenized_ds["train"],
+        eval_dataset=tokenized_ds["test"],
         data_collator=data_collator,
     )
 
@@ -75,10 +49,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-p", "--preprocess", action="store_true", help="Only run the preprocessing."
-    )
-    args = parser.parse_args()
-
-    main(args)
+    main()
